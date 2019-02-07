46ffc9519f447bdee841fcd13173d5b7d0f9b6af
Seolhee Kim <seolheekim808@gmail.com>
2019-02-06 23:05:08 -0800

    created handleSearchSubmit method to request response to search

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  //the Q property is specified to be called Q from the Youtube API Doc.
+  handleSearchSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+    this.setState({ videos: response.data.items })
+  };
+
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar handleSearchSubmit = {this.handleSearchSubmit}/>
+        I have {this.state.videos.length} videos.
       </div>
     )
   }
