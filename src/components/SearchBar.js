e88304b2b3581a58684aed83af96889f87251b4a
Seolhee Kim <seolheekim808@gmail.com>
2019-02-06 21:31:42 -0800

    first push

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { search: '' };
+
+  //remember to assign as an arrow function when it is a callback that is going to pass to some child element.
+  handleInputChange = (event) => {
+    this.setState({ search : event.target.value })
+  };
+
+  handleFormSubmit = (event ) => {
+
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit = {this.handleFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.search}
+              onChange = { this.handleInputChange }
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default SearchBar;
